639804f43b00e698be8d9d0484224287dd2243a8
soenneker <jake@soenneker.com>
2026-08-29 14:51:16 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Validators.BasicAuth.Functions/BasicAuthValidator.cs
+++ src/Soenneker.Validators.BasicAuth.Functions/BasicAuthValidator.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace Soenneker.Validators.BasicAuth.Functions;
 
-///<inheritdoc cref="IBasicAuthValidator"/>
+/// <inheritdoc cref="IBasicAuthValidator"/>
 public sealed class BasicAuthValidator : Validator.Validator, IBasicAuthValidator
 {
     private readonly IConfiguration _configuration;
